74d8b78b5480eaad6614868bbd2c342ebc010e4c
Singhharshitcod <har53840@gmail.com>
2026-05-16 23:21:03 +0530

    Add files via upload

---
+++ personal_intro.py
@@ -0,0 +1,15 @@
+def task1():
+    print("--- Welcome to the Personal Intro Program ---\n")
+    name = input("What is your name? ")
+    age = input("How old are you? ")
+    hobby = input("What is your favorite hobby? ")
+
+   
+    print("\n--- Your Profile ---")
+    print(f"Hello, {name}! Welcome aboard.")
+    print(f"It is amazing that at {age} years old, you are so passionate about {hobby}.")
+    print("Keep up the great work!")
+
+
+if __name__ == "__main__":
+    task1()
